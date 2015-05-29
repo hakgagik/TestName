ed8f4d2416d13e450687e1175da3c4cb8a36b3ca
hakgagik <xddarkgalegh@aim.com>
2015-05-29 14:18:18 -0400

    Added main skeleton

---
+++ TestName/Canvas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestName
+{
+    class Canvas
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Canvas(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+
+        public void DrawBackground()
+        {
+            
+        }
+
+        public void DrawCircle(float x, float y, float r)
+        {
+            
+        }
+
+        public void DrawSqaure(float x, float y, float s)
+        {
+            
+        }
+
+        public void Resize(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ TestName/GameMode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace TestName
+{
+    class GameMode : GameWindow
+    {
+        // Main gameplayController
+        private GameplayController gameplayController;
+
+        // Main environment
+        private Environment environment;
+
+        // Main towerController
+        
+        // Main minionController
+
+        // Main canvas object. This is currently the game's only main View.
+        private Canvas canvas;
+
+        public GameMode() : base()
+        {
+            Load += OnLoad;
+            Resize += OnResize;
+            UpdateFrame += OnUpdateFrame;
+            RenderFrame += OnRenderFrame;
+        }
+
+        public GameMode(int length, int width) : base(length, width)
+        {
+            Load += OnLoad;
+            Resize += OnResize;
+            UpdateFrame += OnUpdateFrame;
+            RenderFrame += OnRenderFrame;
+        }
+
+        private void OnLoad(object e, EventArgs sender)
+        {
+            VSync = VSyncMode.On;
+            // TODO: Loading code here
+            gameplayController = new GameplayController();
+        }
+
+        private void OnResize(object e, EventArgs sender)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            canvas.Resize(Width, Height);
+        }
+
+        private void OnUpdateFrame(object e, EventArgs sender)
+        {
+            if (Keyboard[Key.Escape])
+            {
+                Exit();
+            }
+
+            // TODO: All game logic goes here
+            gameplayController.Update();
+        }
+
+        private void OnRenderFrame(object e, EventArgs sender)
+        {
+            // TODO: All drawing code goes here
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+
+            environment.Draw(canvas);
+
+
+
+            SwapBuffers();
+        }
+    }
+}
