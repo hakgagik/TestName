7f5ce3919ce4ea8bbe2f170954d480ce2819eeea
AtrumDelorox <dleoshadow@gmail.com>
2015-05-29 14:34:52 -0400

    SomeComment

---
+++ TestName/MinionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestName
+{
+    class MinionController
+    {
+        public void moveTo()
+        {
+
+        }
+        public void geMiniontPosition()
+        {
+            //call function
+        }
+        public void setMinionPosition()
+        {
+
+        }
+        public void setMinionSpawnPosition() 
+        {
+            //call outside function
+        }
+        public void  takeDamage() 
+        {
+
+        }
+        update();
+        draw();
+    }
+}
---
+++ TestName/Minions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestName
+{
+    class Minions
+    {
+        //attack damage
+        int attackDamage;
+        //attack speed
+        float attackSpeed;
+        //health
+        int minionHealth;
+        //move speed
+        float moveSpeed;
+        //velocity
+        float minionVelocity;
+        //Minions
+        static ArrayList minions = new ArrayList();
+        public Minions()
+        {
+            attackDamage = 5;
+            attackSpeed = 1;
+            minionHealth = 100;
+            moveSpeed = 2;
+            minionVelocity = 1;
+        }
+        public static void spawnMinion()
+        {
+            char i = 'A';
+            minions.Add(i);
+            i++;
+            if (i == '[')
+                i = 'a';
+        }
+        public ArrayList sendMinionData()
+        {
+            return minions;
+        }
+    }
+}
